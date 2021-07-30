4ecf76a815deb3fac5cc0868cbc8c1a473a24a37
박규혁(Kyuhyeok Park)/상품관리개발팀/11ST <gyugyu@sk.com>
2021-07-30 21:46:58 +0900

    initial commit

---
+++ src/test/kotlin/com/gyugyu/blog/BlogApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.gyugyu.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
